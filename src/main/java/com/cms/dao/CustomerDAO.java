a35d28d2c613a5e14c786720090d8b8ad60bfdba
Jeyandhan <86904129+Jeyandh@users.noreply.github.com>
2024-01-19 16:18:18 +0530

    Update CustomerDAO.java

--- src/main/java/com/cms/dao/CustomerDAO.java
+++ src/main/java/com/cms/dao/CustomerDAO.java
@@ -1,8 +1,8 @@
-package com.cms.DAO;
+package com.cms.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.cms.BEAN.Customer;
+import com.cms.bean.Customer;
 
 public interface CustomerDAO extends CrudRepository<Customer, Integer>{
 
